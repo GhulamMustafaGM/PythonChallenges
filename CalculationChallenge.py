968c947b41bb4514bb77ef3af00cd42d3ca8e872
Ghulam Mustafa <mustafaji@gmail.com>
2021-01-12 09:19:57 +0100

    Calculations challenge

---
+++ CalculationChallenge.py
@@ -0,0 +1,18 @@
+p = input('Enter value for p:')
+n = input('Enter value for n:')
+r = input('Enter value for r:')
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+si = (p*n*r)/100
+
+print(si)
+
+# output
+
+# Enter value for p:1000
+# Enter value for n:1
+# Enter value for r:10
+# 100.0
